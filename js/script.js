468565d20ca30a6648ad8f42a56b1f4c8cf0e259
Lidia <60134376+tkadlets@users.noreply.github.com>
2020-03-07 23:48:58 +1000

    правки

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
  var checkInStorage = localStorage.getItem("checkInStorage");
  var checkOutStorage = localStorage.getItem("checkOutStorage");
 
- searchForm.classList.remove("search-form-shown"); 
+ searchForm.classList.remove("search-form-shown");
 
  hotelSearchButton.addEventListener("click", function (evt) {
  	evt.preventDefault();
@@ -22,10 +22,10 @@
  	if (kidsStorage) {
 		kidsNumber.value = kidsStorage;
 }
-	if (kidsStorage) {
+	if (checkInStorage) {
 		checkIn.value = checkInStorage;
 }
-	if (kidsStorage) {
+	if (checkOutStorage) {
 		checkOut.value = checkOutStorage;
 }
  });
@@ -34,10 +34,29 @@ searchForm.addEventListener("submit", function (evt) {
  if (!checkIn.value || !checkOut.value || !adultsNumber.value || !kidsNumber.value) {
  	evt.preventDefault();
  	console.log("Отсутствующее(-ие) значение(-я)");
- 	searchForm.classList.add("search-form-incorrect");
  } else {
  		localStorage.setItem("checkInStorage", checkIn.value);
 		localStorage.setItem("checkOutStorage", checkOut.value);
+ }
+ if (!checkIn.value) {
+ 	checkIn.style.border = "2px solid #d44848";
+ } else {
+ 	checkIn.style.border = "";
+ }
+ if (!checkOut.value) {
+ 	checkOut.style.border = "2px solid #d44848";
+ }	else {
+ 	checkOut.style.border = "";
+ }
+ if (!adultsNumber.value) {
+ 	adultsNumber.style.border = "2px solid #d44848";
+ }	else {
+ 	adultsNumber.style.border = "";
+ }
+ if (!kidsNumber.value) {
+ 	kidsNumber.style.border = "2px solid #d44848";
+ }	else {
+ 	kidsNumber.style.border = "";
  }
 });
 
@@ -52,3 +71,15 @@ searchForm.addEventListener("submit", function (evt) {
 	}
 });
 
+function initMap() {
+    var sedona = {lat: 34.869, lng: -111.761};
+    var map = new google.maps.Map(
+    document.getElementById('map'), {
+    	zoom: 9,
+    	center: sedona,
+    	zoomControl: false,
+    	streetViewControl: false
+    });
+    var marker = new google.maps.Marker({
+    	position: sedona, map: map});
+ }
